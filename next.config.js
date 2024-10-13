476cd6e8a56df1db550ad30931cab9719d00b5ad
omar faruk <69864169+whereisfarukk@users.noreply.github.com>
2024-10-14 01:03:25 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   output: 'export', // Make sure you're exporting static files
-  assetPrefix: process.env.GITHUB_PAGES ? '/whereisfarukk.github.io/' : '', // Set this to your repo name
+  assetPrefix: process.env.GITHUB_PAGES ? '/your-repo-name/' : '', // Set this to your repo name
 };
