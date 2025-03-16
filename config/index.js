5326cb3584dac8f3cb745fa0e0716dda758900ae
whereisfarukk <whereisfarukk@gmail.com>
2025-03-16 16:08:56 +0600

    feat: :sparkles: Added mail sending feature
    Added mail sending service in contact .Used mailjs for sending mails.

--- config/index.js
+++ config/index.js
@@ -1,9 +1,7 @@
-
-
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = ""
+export const EMAILJS_TEMPLATE_ID = "template_s5oddvs";
 
-export const EMAILJS_SERVICE_ID = ""
+export const EMAILJS_SERVICE_ID = "service_aqk17bl";
 
-export const EMAILJS_PUBLIC_KEY = ""
+export const EMAILJS_PUBLIC_KEY = "IfC5ItZ6_rxGXOCoc";
